e989063ff92aea89b4e091a7cb7bcff8bbcd0f61
offir111 <offir111@gmail.com>
2026-06-07 16:06:52 +0300

    Fix navbar: move robots button next to main pages

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -48,8 +48,6 @@ export default function NavBar({ page, navigate }) {
           </button>
         ))}
 
-        <span className="nav-sep" />
-
         {/* More dropdown */}
         <div className="nav-more-wrap" ref={menuRef}>
           <button
